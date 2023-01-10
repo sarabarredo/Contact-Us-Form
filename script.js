d925b6c5407affd2183c36d9033b2f29ff74c71a
Sara Barredo <sarabarredo2000@gmail.com>
2023-01-10 11:40:03 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const inputs = document.querySelectorAll('.input');
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add('focus');
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if (this.value == '') {
+        parent.classList.remove('focus');
+    }
+    
+}
+
+inputs.forEach(inputs => {
+    inputs.addEventListener('focus', focusFunc);
+    inputs.addEventListener('blur', blurFunc);
+});
